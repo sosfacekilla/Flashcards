3c0f6906350d374a7ec6503dccbd5244087f33ab
Alexandra Sos <SOSfaceKILLA@Macintosh-78.local>
2014-09-10 14:52:46 -0700

    routing

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -43,7 +43,7 @@ class WordsController < ApplicationController
 	def destroy
 		@word = Word.find(params[:id])
 		@word.destroy
-		redirect_to topic_words_path
+		redirect_to topics_path
 	end
 
 	private
